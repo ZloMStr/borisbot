2b43815fc599f9cac98096483c080cbdabfb4f2f
ZloMStr <zlomstr@gmail.com>
2022-12-07 15:05:28 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/webhook', [\App\Http\Controllers\WebhookController::class, 'input']);
+Route::post('/webhook', [\App\Http\Controllers\WebhookController::class, 'input']);
